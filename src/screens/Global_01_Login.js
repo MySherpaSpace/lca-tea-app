3f10c3cd5dadccdf6a89a0e12b5580abd77504e6
Maria Reyes <16553376+marijorc@users.noreply.github.com>
2022-11-16 17:58:12 -0500

    Updated login page

--- src/screens/Global_01_Login.js
+++ src/screens/Global_01_Login.js
@@ -1,40 +1,53 @@
-import React from 'react';
+import "./Global_01_Login.css"
+import React, { useState } from 'react';
  
 const LoginPg = (props) => {
+  const [userName, setUserName] = useState("")
+  const [userPassword, setUserPassword] = useState("")
   const onLoginPress = () => props.onLogin("home")
+
+  const [rememberMe, setRememberMe] = useState(false)
+
+  const onUsernameEnter = (input) => setUserName(input.target.value)
+  const onPasswordEnter = (input) => setUserPassword(input.target.value)
+
+  const onRememberMePresss = () => setRememberMe(prev => !prev)
   
   return (
-    <div className="ui placeholder segment">
-      <div className="ui two column very relaxed stackable grid">
-        <div className="column">
-          <div className="ui form">
+    <div className='card-container'>
+        <div className="ui centered card">
+          <div style={{paddingTop:20}}>
+            <img className="ui centered circular tiny image" src={require("../img/app-logo.png")}/>
+          </div>
+          <div className='ui centered header'>LCA-TEA App</div>
+          <div className='content'>
+          <form className="ui form" onSubmit={onLoginPress}>
             <div className="field">
               <label>Username</label>
-              <div className="ui left icon input">
-                <input type="text" placeholder="Username"/>
-                <i className="user icon"></i>
-              </div>
+              <input type="text" placeholder="user@email.com" value={userName} onChange={onUsernameEnter}/>
             </div>
             <div className="field">
               <label>Password</label>
               <div className="ui left icon input">
-                <input type="password"/>
+                <input type="password" placeholder='••••' value={userPassword} onChange={onPasswordEnter}/>
                 <i className="lock icon"></i>
               </div>
             </div>
-            <div className="ui blue submit button" onClick={onLoginPress}>Login</div>
+            <div className="field">
+              <div className="ui checkbox">
+                <input type="checkbox" tabIndex="0" checked={rememberMe} onChange={onRememberMePresss}/>
+                <label>Remember Me</label>
+              </div>
+            </div>
+            <div className='ui center aligned container'>
+              <button className="ui button" type="submit">Login</button>
+            </div>
+          </form>
+          </div>
+          <div className='extra center aligned content'>
+            <a className="ui content" onClick={() => alert("Not available")}>Don't have an account? Register</a>
           </div>
         </div>
-        <div className="middle aligned column">
-          <div className="ui big button">
-            <i className="signup icon"></i>
-            Sign Up
-          </div>
-        </div>
-      </div>
-      <div className="ui vertical divider">
-        Or
-      </div>
     </div>
   );
 }
